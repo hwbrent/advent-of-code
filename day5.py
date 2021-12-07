3ce7c8fd63d4e38b0e53092746a647742d0dc7b0
mustcat <hwbrent@icloud.com>
2021-12-07 01:14:40 +0000

    finished day 5

--- day5.py
+++ day5.py
@@ -1,45 +1,133 @@
 from pprint import PrettyPrinter
-pp = PrettyPrinter(indent = 4)
+import math
+pp = PrettyPrinter(indent=4)
+# import numpy as np
 
-class Diagram:
-    def __init__(self, coordinates):
-        # Below comprehension takes points from ['10,11 -> 989,990'] to [[10, 11], [989, 990]]
-        self.coordinates = [[[int(num) for num in coord.split(",")] for coord in entry.split(" -> ")] for entry in coordinates]
-        all_y_coords = {coord[1] for pair in self.coordinates for coord in pair}
-        all_x_coords = {coord[0] for pair in self.coordinates for coord in pair}
-        self.top_left = [min(all_x_coords), max(all_y_coords)]
-        self.top_right = [max(all_x_coords), max(all_y_coords)]
-        self.bottom_left = [min(all_x_coords), min(all_y_coords)]
-        self.bottom_right = [max(all_x_coords), min(all_y_coords)]
-
-        number_of_rows = max(all_y_coords) - min(all_y_coords)
-        number_of_columns = max(all_x_coords) - min(all_x_coords)
-        self.grid = [[0 for x in range(number_of_rows)] for y in range(number_of_columns)]
+class Grid:
+    def __init__(self, line_objs): # line_objs => array of [[x1, y2], [x2, y2]]
+        all_x = [entry for line in line_objs for entry in line.get_axis_coordinates("x")]
+        all_y = [entry for line in line_objs for entry in line.get_axis_coordinates("y")]
+        self.bounds = {
+            "x": [min(all_x), max(all_x)],
+            "y": [min(all_y), max(all_y)]
+        }
+        self.x_offset = min(all_x)
+        self.y_offset = min(all_y)
+        self.grid = [ # each nested array is a row, so it'll need to be of length max(all_x) - min(all_x)
+            [0 for x in range(max(all_x) + 1)] for y in range(max(all_y) + 1)
+        ]
     
-    def process_line(self, line_coordinates):
-        pass
+    def process_flat_line(self, line): # line => [[x1, y2], [x2, y2]]
+        flat_direction = line.direction()
+        if flat_direction == "x": # x value doesn't change, y does
+            x = line.points[0][0]
+            span = sorted(line.get_axis_coordinates("y"))
+            # print("x unchanging - spanning y:", span)
+            # for y in range(span[0], span[1]): # +1 ?
+            for y in range(span[0], span[1] + 1):
+                # print(f"[{x},{y}] += 1")
+                self.grid[y][x] += 1
+        if flat_direction == "y": # y doesn't change, x does
+            y = line.points[0][1]
+            span = sorted(line.get_axis_coordinates("x"))
+            # print("y unchanging - spanning x:", span)
+            # for x in range(span[0], span[1]): # +1 ?
+            for x in range(span[0], span[1] + 1):
+                # print(f"[{x},{y}] += 1")
+                self.grid[y][x] += 1
+    
+    def overlaps(self, threshold):
+        count = 0
+        for row in self.grid:
+            for value in row:
+                if value >= threshold:
+                    count += 1
+        return count
 
+class Line:
+    def __init__(self, line): # line => [[x1, y2], [x2, y2]]
+        self.points = line
 
-def part1():
+    def is_flat(self):
+        [x1, y1], [x2, y2] = self.points
+        return (x1 == x2) or (y1 == y2)
+    
+    def direction(self):
+        x1 = self.points[0][0]
+        x2 = self.points[1][0]
+        if x1 == x2:
+            return "x" # the line is a flat vertical line
+        else: # y1 == y2
+            return "y" # the line is a flat horizontal line
+        
+    def get_axis_coordinates(self, axis): # axis => "x" or "y"
+        [x1, y1], [x2, y2] = self.points
+        if axis == "x":
+            return [x1, x2]
+        else: # axis == "y":
+            return [y1, y2] 
+
+class Grid2(Grid):
+    def process_diagonal_line(self, line):
+        direction = line.diagonal_direction()
+        current_point, end_point = line.points
+        while current_point != end_point:
+            x, y = current_point
+            self.grid[y][x] += 1
+            current_point[0] += direction[0]
+            current_point[1] += direction[1]
+        x, y = current_point
+        self.grid[y][x] += 1
+
+class Line2(Line):
+    def is_diagonal(self):
+        [x1, y1], [x2, y2] = self.points
+        return abs(x2 - x1) / abs(y2 - y1) == 1
+
+    def diagonal_direction(self):
+        [x1, y1], [x2, y2] = self.points
+        if (x1 > x2) and (y1 < y2):
+            return [-1, 1]
+        elif (x1 < x2) and (y1 < y2):
+            return [1, 1]
+        elif (x1 > x2) and (y1 > y2):
+            return [-1, -1]
+        else: # if (x1 < x2) and (y1 > y2):
+            return [1, -1]
+
+def part1(lines):
+    line_objs = [Line(line) for line in lines if Line(line).is_flat()]
+    grid = Grid(line_objs)
+    for line in line_objs:
+        grid.process_flat_line(line)
+    return grid.overlaps(2)
+    
+def part2(lines):
+    line_objs = [Line2(line) for line in lines]
+    grid = Grid2(line_objs)
+    for line in line_objs:
+        if line.is_flat():
+            grid.process_flat_line(line)
+        elif line.is_diagonal():
+            grid.process_diagonal_line(line)
+        else:
+            continue
+    return grid.overlaps(2)
+
+def main():
     with open("./inputs/day5.txt") as infile:
         infile = infile.read().split("\n")[:-1] # length 500
     
-    diagram = Diagram(infile)
-    pp.pprint(diagram.grid)
+    lines = [
+        [
+            [
+                int(number) for number in entry.split(",")
+            ] for entry in subarray.split(" -> ")
+        ] for subarray in infile
+    ]
 
-    for line in diagram.coordinates:
-        x1, y1 = line[0]
-        x2, y2 = line[1]
-        if not ((x1 == x2) or (y1 == y2)):
-            continue
-        else:
-            # print(line)
-            continue
-    
-
-def part2():
-    pass
+    print("Answer for part 1 =", part1(lines))
+    print("Answer for part 2 =", part2(lines))
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    main()
---
+++ day6.py
@@ -0,0 +1,15 @@
+def part1():
+    pass
+
+def part2():
+    pass
+
+def main():
+    with open("./inputs/day6.txt") as infile:
+        infile = infile.read().strip().split(",")
+    
+    # print("Answer for part 1 =", part1())
+    # print("Answer for part 2 =", part2())
+
+if __name__ == "__main__":
+    main()
