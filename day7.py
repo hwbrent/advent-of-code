73f3a7e5ce306e0dc9cb47a62ea6670772e59b71
mustcat <hwbrent@icloud.com>
2021-12-11 19:50:02 +0000

    finished day 7

--- day7.py
+++ day7.py
@@ -1,15 +1,16 @@
-def part1():
-    pass
+def part1(param):
+    return min([sum([abs(num - point) for num in param]) for point in range(min(param), max(param) + 1)])
 
-def part2():
-    pass
+def part2(param):
+    # really slow, but works
+    return min([sum([sum(range(1, abs(num - point) + 1)) for num in param]) for point in range(min(param), max(param) + 1)])
 
 def main():
     with open("./inputs/day7.txt") as infile:
-        infile = infile.read()
+        infile = [int(number) for number in infile.read().split(",")]
     
-    print("Answer for part 1 =", part1())
-    print("Answer for part 2 =", part2())
+    print("Answer for part 1 =", part1(infile))
+    print("Answer for part 2 =", part2(infile))
 
 if __name__ == "__main__":
     main()
