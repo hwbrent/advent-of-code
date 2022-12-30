75b7dd15ae297e21ce4d339072eeade8461f45df
mustcat <hwbrent@icloud.com>
2022-12-30 09:05:33 +0000

    Run file-utils

---
+++ 2022/python/day14.py
@@ -0,0 +1,17 @@
+from utils import get_input
+from pprint import PrettyPrinter
+pp = PrettyPrinter(indent=4)
+
+# Puzzle description: https://adventofcode.com/2022/day/14
+# Puzzle input:       https://adventofcode.com/2022/day/14/input
+
+def part1(input):
+    print('Part 1 -->', None)
+
+def part2(input):
+    print('Part 2 -->', None)
+
+if __name__ == '__main__':
+    input = get_input()
+    part1(input)
+    part2(input)
