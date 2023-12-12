867f13f9b07a6deb7121d18ea76c32646a4084d9
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-12-12 15:02:50 +0000

    Remove unneeded functions

--- utils.py
+++ utils.py
@@ -30,35 +30,6 @@ def get_most_recent_year() -> int:
     return year
 
 
-def get_year_page_url(year: int = get_most_recent_year()) -> str:
-    """
-    Gets the URL for the most recent year of AOC if `year` is not specified,
-    else gets the year specified by `year`.
-    """
-    year = str(year)
-
-    url = urllib.parse.urljoin(AOC_BASE_URL, year)
-
-    return url
-
-
-def get_year_page_html(year: int = get_most_recent_year()) -> str:
-    """
-    Gets the HTML for the most recent year of AOC if `year` is not specified,
-    else gets page for the the year specified by `year`.
-    """
-
-    url = get_year_page_url(year)
-
-    response = requests.get(url)
-
-    if not response.ok:
-        error_message = f"{url} -- {response.status_code} -- {response.reason}"
-        raise Exception(error_message)
-
-    return response.text
-
-
 def is_valid_year(year: int) -> bool:
     return EARLIEST_YEAR <= year <= get_most_recent_year()
 
