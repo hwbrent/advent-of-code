297c692999209509d09c1bdddb07954f91e41703
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-12-12 14:09:48 +0000

    Add function to get most recent year that AOC has taken place

--- utils.py
+++ utils.py
@@ -1,3 +1,23 @@
+import datetime
+
 # The "home page" as it were
 # Seems to show the problems for the most recent year by default
 AOC_BASE_URL = "https://adventofcode.com/"
+
+# The number of the month of December
+DECEMBER = 12
+
+
+def get_most_recent_year() -> int:
+    """
+    Returns the recent year that AOC has taken place. i.e. if it's December
+    of the current year, this function will return the current year. Otherwise,
+    it returns the previous year/
+    """
+    now = datetime.datetime.now()
+    year = now.year
+
+    if now.month != DECEMBER:
+        year -= 1
+
+    return year
