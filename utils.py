937863eac0e2ef1b31a9b3f4aa8bc38082de332e
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-12-12 15:00:29 +0000

    Add function to parse command-line args for year and day

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import sys
 import datetime
 import urllib.parse
+
 import requests
 
 # The "home page" as it were
@@ -8,6 +10,9 @@ AOC_BASE_URL = "https://adventofcode.com/"
 
 # The number of the month of December
 DECEMBER = 12
+
+# The first year that they did AOC.
+EARLIEST_YEAR = 2015
 
 
 def get_most_recent_year() -> int:
@@ -52,3 +57,102 @@ def get_year_page_html(year: int = get_most_recent_year()) -> str:
         raise Exception(error_message)
 
     return response.text
+
+
+def is_valid_year(year: int) -> bool:
+    return EARLIEST_YEAR <= year <= get_most_recent_year()
+
+
+def is_valid_day(day: int) -> bool:
+    return 1 <= day <= 25
+
+
+def parse_args() -> tuple[int, int]:
+    """
+    Basically just checks the command-line arguments for any valid combination
+    of year and/or day (or neither). If the arguments are valid, this returns
+    a tuple of the form (year, day)
+    """
+    args = sys.argv[1:]
+
+    now = datetime.datetime.now()
+
+    year = None
+    day = None
+
+    match len(args):
+        case 0:
+            # Assume we want to generate a file for TODAY
+            year = now.year
+            day = now.day
+        case 1:
+            # First check if the argument is actually a number.
+            # Then figure out if it's a year or a day
+            number = args[0]
+
+            if not number.isnumeric():
+                raise Exception(
+                    f"Argument must be a number. Received type: {type(number)}"
+                )
+
+            # Remove any leading zeroes
+            number = number.lstrip("0")
+
+            match len(number):
+                case 4:
+                    # The number must be a year.
+                    # Now check that it's a valid year
+                    number = int(number)
+                    if not is_valid_year(number):
+                        error_message = f"Year must be between {EARLIEST_YEAR} and {get_most_recent_year()}. Received: {number}"
+                        raise Exception(error_message)
+
+                    # Assume that the day is today
+                    year = number
+                    day = now.day
+                case 1 | 2:
+                    number = int(number)
+                    if not is_valid_day(number):
+                        error_message = (
+                            f"Day must be between 1 and 25. Received: {number}"
+                        )
+                        raise Exception(error_message)
+
+                    day = number
+                    year = get_most_recent_year()
+                case _:
+                    raise Exception(
+                        f"Invalid number of digits. Received: {len(number)}"
+                    )
+
+        case 2:
+            # Either (year,day) or (day, year)
+            num1, num2 = args[0:2]
+            num1 = int(num1.lstrip("0"))
+            num2 = int(num2.lstrip("0"))
+
+            if is_valid_year(num1) and is_valid_day(num2):
+                year = num1
+                day = num2
+            elif is_valid_year(num2) and is_valid_day(num1):
+                year = num2
+                day = num1
+            else:
+                error_message = f"Invalid arguments. Received: {num1}, {num2}"
+                raise Exception(error_message)
+
+        case _:
+            error_message = (
+                f"Invalid number of arguments: {len(args)}. Expected 0, 1 or 2"
+            )
+            raise Exception(error_message)
+
+    return year, day
+
+
+def main():
+    print(parse_args())
+
+
+if __name__ == "__main__":
+    main()
