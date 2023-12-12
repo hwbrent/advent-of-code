ab68b49876aac8bbad7b5b0934541009fcd13771
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-12-12 14:23:42 +0000

    Add function to get html for AOC year

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import datetime
 import urllib.parse
+import requests
 
 # The "home page" as it were
 # Seems to show the problems for the most recent year by default
@@ -34,3 +35,20 @@ def get_year_page_url(year: int = get_most_recent_year()) -> str:
     url = urllib.parse.urljoin(AOC_BASE_URL, year)
 
     return url
+
+
+def get_year_page_html(year: int = get_most_recent_year()) -> str:
+    """
+    Gets the HTML for the most recent year of AOC if `year` is not specified,
+    else gets page for the the year specified by `year`.
+    """
+
+    url = get_year_page_url(year)
+
+    response = requests.get(url)
+
+    if not response.ok:
+        error_message = f"{url} -- {response.status_code} -- {response.reason}"
+        raise Exception(error_message)
+
+    return response.text
