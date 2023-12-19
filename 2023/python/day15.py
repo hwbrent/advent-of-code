f1d8d4befadc69ca9f96ce1d30e6c19f79fb5723
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-12-19 20:38:03 +0000

    Move HASH logic into its own function

--- 2023/python/day15.py
+++ 2023/python/day15.py
@@ -169,26 +169,30 @@ def parse_raw_input(input: str):
     return input.strip().split(",")
 
 
+def HASH(string):
+    current_value = 0
+
+    for char in string:
+        # Determine the ASCII code for the current character of the
+        # string.
+        # Increase the current value by the ASCII code you just determined.
+        current_value += ord(char)
+
+        # Set the current value to itself multiplied by 17.
+        current_value *= 17
+
+        # Set the current value to the remainder of dividing itself by
+        # 256.
+        current_value %= 256
+
+    return current_value
+
+
 def part1(input):
     answer = 0
 
     for string in input:
-        current_value = 0
-
-        for char in string:
-            # Determine the ASCII code for the current character of the
-            # string.
-            # Increase the current value by the ASCII code you just determined.
-            current_value += ord(char)
-
-            # Set the current value to itself multiplied by 17.
-            current_value *= 17
-
-            # Set the current value to the remainder of dividing itself by
-            # 256.
-            current_value %= 256
-
-        answer += current_value
+        answer += HASH(string)
 
     return answer
 
