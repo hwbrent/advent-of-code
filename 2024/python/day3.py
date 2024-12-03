6387dab9e7356dea50a11493e9cefe676c25bdd3
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-12-03 10:27:08 +0000

    Do day 3
    Squashed commit of the following:
    
    commit 566c3d1f8076c0b1f132ebab3ffe666372fe020b
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:25:39 2024 +0000
    
        Use const for "mul" pattern
    
    commit eb5ca3178e99d74bed5fb9b0f52d9f8de5386b3f
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:21:50 2024 +0000
    
        Add function for "mul" `int` retrieval
    
    commit f6f97b20395e9eaa16bde2a27f94030d34492790
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:19:07 2024 +0000
    
        Use finditer
    
    commit 84e37084d90cd96e7f206dbbe023bed6b2eec276
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:17:39 2024 +0000
    
        Add comments and reorder
    
    commit b7073c1d55c5467ac45f14f58749334ec0ea3537
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:08:56 2024 +0000
    
        Clean up a bit
    
    commit 21773f84368528dd4ded3cdc17145bd6bbd89a0d
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 10:05:12 2024 +0000
    
        Do part 2
    
    commit 98551f7b5f989a9249e236ee5d281ae31b82455b
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 09:37:01 2024 +0000
    
        Do part 1
    
    commit 8f785ffb8f0cf0fbba2dc2e12b5d7ae871ed670d
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 3 09:27:30 2024 +0000
    
        Run utils.py

---
+++ 2024/python/day3.py
@@ -0,0 +1,134 @@
+import os
+import sys
+import re
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: https://adventofcode.com/2024/day/3
+# Input URL:   https://adventofcode.com/2024/day/3/input
+
+"""
+--- Day 3: Mull It Over ---
+
+"Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.
+The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+The computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. All of the instructions have been jumbled up!
+It seems like the goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
+However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+For example, consider the following section of corrupted memory:
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+"""
+
+
+def parse_raw_input(input: str) -> str:
+    """
+    Returns the input but with whitespace stripped off
+
+    (The input is just one long line, so no need to split into lines or
+    anything)
+    """
+    return input.strip()
+
+
+# A regex pattern matching a "mul" instruction, with the two ints in groups
+# for easier retrieval
+MUL = r"mul\((\d{1,3}),(\d{1,3})\)"
+
+
+# function to get the two ints from a "mul" instruction
+parse_mul = lambda match: (int(match[1]), int(match[2]))
+
+
+def part1(input) -> int:
+    """
+    1. Create a regex pattern which targets the 'mul(<int>,<int>)' pattern,
+       capturing the two numbers as groups
+    2. Find each instance of that pattern
+    3. For each instance, get the two captured groups, cast them to `int`s,
+       multiply them together, and add that number to a running total
+    """
+    answer = 0
+    matches = re.finditer(MUL, input)
+    for match in matches:
+        num1, num2 = parse_mul(match)
+        answer += num1 * num2
+    return answer
+
+
+def part2(input):
+    """
+    Pretty much the same as part 1, except we look for "do()" and "don't()"
+    as well, and depending on which we find, we skip/proceed with doing the
+    multiplication of the 'mul' values
+    """
+
+    MUL_PREFIX = "mul"
+    DO = "do()"
+    DONT = "don't()"
+
+    answer = 0
+
+    # The most recent instruction found.
+    # True means "do()", False means "don't()"
+    do = True
+
+    # Get the patterns of "do()" and "don't()"
+    instructions = (
+        r"do\(\)",  # for do()
+        r"don't\(\)",  # for don't()
+    )
+
+    # OR the patterns together into one big pattern.
+    # A match will be found if it sees one of the three, meaning we have to
+    # check each match to see which one it was
+    pattern = f"(({MUL})|({instructions[0]})|({instructions[1]}))"
+
+    matches = re.finditer(pattern, input)
+    for match in matches:
+        # Get the string which was found.
+        # It will either be "do()", "don't", or will start with "mul"
+        string = match[0]
+
+        if string.startswith(MUL_PREFIX):
+            # If the most recent instruction was "don't()", we don't bother
+            # with doing any multiplication
+            if not do:
+                continue
+
+            # Get the ints within the "mul" instruction
+            mul = re.match(MUL, string)
+            num1, num2 = parse_mul(mul)
+
+            # Add to the final answer
+            answer += num1 * num2
+
+        elif string == DO:
+            # Set the flag to True
+            do = True
+
+        elif string == DONT:
+            # Set the flag to False
+            do = False
+
+    return answer
+
+
+def main():
+    raw_input = utils.get_raw_input()
+    parsed_input = parse_raw_input(raw_input)
+
+    utils.handle(part1(parsed_input), 1)  # 170807108
+    utils.handle(part2(parsed_input), 2)  # 74838033
+
+
+if __name__ == "__main__":
+    main()
