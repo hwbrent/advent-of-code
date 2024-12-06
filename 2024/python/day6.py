fd0a4beb1357abc7f72348c5266c732ca106ab8c
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-12-06 08:58:15 +0000

    Fix incorrect direction orders

--- 2024/python/day6.py
+++ 2024/python/day6.py
@@ -113,10 +113,11 @@ OBSTACLE = "#"
 
 
 # fmt: off
-UP    = ( 0, -1)
-DOWN  = ( 0,  1)
-LEFT  = (-1,  0)
-RIGHT = ( 1,  0)
+# row direction, column direction
+UP    = (-1,  0)
+DOWN  = ( 1,  0)
+LEFT  = ( 0, -1)
+RIGHT = ( 0,  1)
 # fmt: on
 
 
