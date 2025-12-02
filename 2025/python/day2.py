a816a6af180f39e122a18b86da40cd7440b9b34c
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-02 08:51:18 +0000

    2025 day 2 parts 1+2
    Squashed commit of the following:
    
    commit b5567501eb6e7f6317491f4d2cd8658f11bac17b
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:50:32 2025 +0000
    
        Get rid of pointless padding
    
    commit 819d8c9532e4670e12dfcd0c2da32ca31a143ed4
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:44:21 2025 +0000
    
        Only keep helpful print statements
    
    commit 869c9946dff3e371e63a14b89de833d23d7b0f71
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:40:20 2025 +0000
    
        Finish part 2
    
    commit 1b24ee0a5f16bc6dae0e9b9668cd04b93fe231ff
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:39:26 2025 +0000
    
        Comment out example input
    
    commit 3681d281b046b55a80fb529c7c96287dfcc0bd4b
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:38:59 2025 +0000
    
        Add missing 'break'
    
    commit 0ff123a19ecf91e230a950dcdfe1336026531d7b
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:34:34 2025 +0000
    
        Fix incorrect operator
    
    commit c8dc6aa6398deb8fe8ed2447b0cf450d871668f6
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:25:38 2025 +0000
    
        Skip if we can't get evenly-sized substrings
    
    commit 9494fa0a91db5fd16820f9afb20ff3bacac34cc6
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:17:45 2025 +0000
    
        (2) Wrong answer for part 2
    
        63829513081 (too low)
    
    commit 53164313f8a1ebcbafd30d4b80dfaa9e16565dc9
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:15:24 2025 +0000
    
        Remove commented-out code
    
    commit 8fc6d8a51183bd2efdc3eb6eab9d70f9e91aab36
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:14:26 2025 +0000
    
        Wrong answer for part 2
    
        638266240747 (too high)
    
    commit 3341a556c5b59931f381e9fbb5c5df2f88e63068
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 08:00:25 2025 +0000
    
        Finish part 1
    
    commit 039ff5730df2c0eb39e7f19e7e26fd2a4097e7dd
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:59:03 2025 +0000
    
        Add test input
    
    commit a5be28521bc0a50629032be85acca2edfa28cd25
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:56:29 2025 +0000
    
        Fix bad padding
    
    commit 56e4296f53169f823cfaf633dcc67c46d0103574
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:44:14 2025 +0000
    
        Ensure numbers are of equal length
    
    commit 3e1e9aff18d465ea6974327edbb923f91da5813d
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:31:06 2025 +0000
    
        Implement parse_raw_input
    
    commit d4b2068be027d3838ae0ecb6f41482948fba2b70
    Merge: 9c2ab03 55aca34
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:25:05 2025 +0000
    
        Merge branch 'main' into 2025--02
    
    commit 9c2ab0315d7c0fb1e26f1adda68302b6ce8c31b4
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 07:23:03 2025 +0000
    
        Add files

---
+++ 2025/python/day2.py
@@ -0,0 +1,135 @@
+import os
+import sys
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: https://adventofcode.com/2025/day/2
+# Input URL:   https://adventofcode.com/2025/day/2/input
+
+"""
+--- Day 2: Gift Shop ---
+
+You get inside and take the elevator to its only other stop: the gift shop. "Thank you for visiting the North Pole!" gleefully exclaims a nearby sign. You aren't sure who is even allowed to visit the North Pole, but you know you can access the lobby through here, and from there you can access the rest of the North Pole base.
+
+As you make your way through the surprisingly extensive selection, one of the clerks recognizes you and asks for your help.
+
+As it turns out, one of the younger Elves was playing on a gift shop computer and managed to add a whole bunch of invalid product IDs to their gift shop database! Surely, it would be no trouble for you to identify the invalid product IDs for them, right?
+
+They've even checked most of the product ID ranges already; they only have a few product ID ranges (your puzzle input) that you'll need to check. For example:
+
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+
+(The ID ranges are wrapped here for legibility; in your input, they appear on a single long line.)
+
+The ranges are separated by commas (,); each range gives its first ID and last ID separated by a dash (-).
+
+Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+
+None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+Your job is to find all of the invalid IDs that appear in the given ranges. In the above example:
+
+    11-22 has two invalid IDs, 11 and 22.
+    95-115 has one invalid ID, 99.
+    998-1012 has one invalid ID, 1010.
+    1188511880-1188511890 has one invalid ID, 1188511885.
+    222220-222224 has one invalid ID, 222222.
+    1698522-1698528 contains no invalid IDs.
+    446443-446449 has one invalid ID, 446446.
+    38593856-38593862 has one invalid ID, 38593859.
+    The rest of the ranges contain no invalid IDs.
+
+Adding up all the invalid IDs in this example produces 1227775554.
+
+What do you get if you add up all of the invalid IDs?
+"""
+
+Input = list[str, str]
+
+
+def parse_raw_input(input: str) -> Input:
+    # input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    return [[int(num) for num in pair.split("-")] for pair in input.strip().split(",")]
+
+
+def part1(input: Input):
+    answer = 0
+    for first_id, last_id in input:
+        # print([first_id, last_id])
+
+        full_range = range(first_id, last_id + 1)
+        for id in full_range:
+            id = str(id)
+            length = len(id)
+            first_half = id[: length // 2]
+            second_half = id[length // 2 :]
+            if first_half == second_half:
+                # print("    ", id)
+                answer += int(id)
+            else:
+                pass
+
+    return answer
+
+
+def part2(input):
+    answer = 0
+    for first_id, last_id in input:
+        # print([first_num, last_num])
+
+        full_range = range(first_id, last_id + 1)
+        for id in full_range:
+            id = str(id)
+
+            # cant repeat if it's bigger than half the length of the entire
+            # id
+            max_substring_length = len(id) // 2
+            for substring_length in range(1, max_substring_length + 1):
+                # split the number up into substrings of length 'substring_length'
+
+                # if we can't get evenly-sized substrings, skip.
+                # e.g. substrings of length 4 within an id of length 6
+                # wouldn't work
+                if len(id) % substring_length != 0:
+                    continue
+
+                substrings = set()
+                first_idx = 0
+                last_idx = substring_length
+                while last_idx <= len(id):
+                    substring = id[first_idx:last_idx]
+                    substrings.add(substring)
+                    first_idx += substring_length
+                    last_idx += substring_length
+
+                if len(substrings) == 1:
+                    answer += int(id)
+                    # print("    ", id)
+
+                    # we already found one way in which a repeating can fit
+                    # into 'id', so it's invalid and we shouldn't check for
+                    # more ways
+                    # e.g. if we have 222222, you can have repeating
+                    # substrings of '2', '22', '222', but we only want to
+                    # account for 222222 once
+                    break
+
+    return answer
+
+
+def main():
+    utils.handle(part1)  # 54234399924 (0.9478640556335449 seconds)
+    utils.handle(part2)  # 70187097315 (7.794951915740967 seconds)
+
+
+if __name__ == "__main__":
+    main()
