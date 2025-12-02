1ddf66b505c723c53bb5b3e493ddbd4bf269dedf
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-02 08:55:06 +0000

    2025 day 1 part 1
    Squashed commit of the following:
    
    commit 4f276e8a43bcce7b621ee9613ace107a50a79acf
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:59:17 2025 +0000
    
        Finish part 1
    
    commit b8590d56fcc9a272de6da0b11fa70bc1b4a33abd
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:53:45 2025 +0000
    
        Implement parse_raw_input
    
    commit 634d4ea2b36f7bd114f4f4549ba790a796368f5e
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:51:44 2025 +0000
    
        Disable inline copilot suggestions
    
    commit 6cdd28b6212cd4387e20db088e223a6abfc618a3
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:46:58 2025 +0000
    
        Create files
    
        I tried the script once but it didnt work. i didnt try again cos the creator asked on reddit for us to not make loads of automated requests to the server. so i just manually copied this stuff over. not too much of a hassle

---
+++ 2025/python/day1.py
@@ -0,0 +1,142 @@
+import os
+import sys
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: {problem_url}
+# Input URL:   {input_url}
+
+"""
+--- Day 1: Secret Entrance ---
+
+The Elves have good news and bad news.
+
+The good news is that they've discovered project management! This has given them the tools they need to prevent their usual Christmas emergency. For example, they now know that the North Pole decorations need to be finished soon so that other critical tasks can start on time.
+
+The bad news is that they've realized they have a different emergency: according to their resource planning, none of them have any time left to decorate the North Pole!
+
+To save Christmas, the Elves need you to finish decorating the North Pole by December 12th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+You arrive at the secret entrance to the North Pole base ready to start decorating. Unfortunately, the password seems to have been changed, so you can't get in. A document taped to the wall helpfully explains:
+
+"Due to new security protocols, the password is locked in the safe below. Please see the attached document for the new combination."
+
+The safe has a dial with only an arrow on it; around the dial are the numbers 0 through 99 in order. As you turn the dial, it makes a small click noise as it reaches each number.
+
+The attached document (your puzzle input) contains a sequence of rotations, one per line, which tell you how to open the safe. A rotation starts with an L or R which indicates whether the rotation should be to the left (toward lower numbers) or to the right (toward higher numbers). Then, the rotation has a distance value which indicates how many clicks the dial should be rotated in that direction.
+
+So, if the dial were pointing at 11, a rotation of R8 would cause the dial to point at 19. After that, a rotation of L19 would cause it to point at 0.
+
+Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
+
+So, if the dial were pointing at 5, a rotation of L10 would cause it to point at 95. After that, a rotation of R5 could cause it to point at 0.
+
+The dial starts by pointing at 50.
+
+You could follow the instructions, but your recent required official North Pole secret entrance security training seminar taught you that the safe is actually a decoy. The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+
+For example, suppose the attached document contained the following rotations:
+
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+
+Following these rotations would cause the dial to move as follows:
+
+    The dial starts by pointing at 50.
+    The dial is rotated L68 to point at 82.
+    The dial is rotated L30 to point at 52.
+    The dial is rotated R48 to point at 0.
+    The dial is rotated L5 to point at 95.
+    The dial is rotated R60 to point at 55.
+    The dial is rotated L55 to point at 0.
+    The dial is rotated L1 to point at 99.
+    The dial is rotated L99 to point at 0.
+    The dial is rotated R14 to point at 14.
+    The dial is rotated L82 to point at 32.
+
+Because the dial points at 0 a total of three times during this process, the password in this example is 3.
+
+Analyze the rotations in your attached document. What's the actual password to open the door?
+"""
+
+
+INITIAL_DIAL_VALUE = 50
+TOTAL_DIAL_VALUES = 100
+
+
+def parse_raw_input(input: str):
+    #     input = """
+    # L68
+    # L30
+    # R48
+    # L5
+    # R60
+    # L55
+    # L1
+    # L99
+    # R14
+    # L82
+    # """
+    out = []
+
+    for line in input.split(os.linesep):
+        line = line.strip()
+        if not line:
+            continue
+
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == "L":
+            distance *= -1
+
+        out.append(distance)
+
+    return out
+
+
+def part1(input):
+    dial_value = INITIAL_DIAL_VALUE
+
+    times_hit_zero = 0
+
+    for direction in input:
+        dial_value += direction
+        dial_value %= TOTAL_DIAL_VALUES  # wrap around
+
+        if dial_value == 0:
+            times_hit_zero += 1
+
+    answer = times_hit_zero
+    return answer
+
+
+def part2(input):
+    answer = None
+    return answer
+
+
+def main():
+    utils.handle(part1)  # 989 (0.0020248889923095703 seconds)
+    utils.handle(part2)
+
+
+if __name__ == "__main__":
+    main()
