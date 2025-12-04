669930c3c99dd6325d6f40ac84488c9f7f275c04
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-04 07:35:01 +0000

    Make logic reusable

--- 2025/python/day4.py
+++ 2025/python/day4.py
@@ -38,52 +38,47 @@ def parse_raw_input(input: str) -> Grid:
     return [[char for char in line.strip()] for line in input.strip().split(os.linesep)]
 
 
+def can_access_roll(grid: Grid, i_line: int, i_space: int) -> bool:
+    if grid[i_line][i_space] != ROLL_OF_PAPER:
+        return False
+
+    len_grid = len(grid)
+    len_line = len(grid[0])
+
+    line_prev = grid[i_line - 1] if i_line - 1 >= 0 else []
+    line_here = grid[i_line]
+    line_next = grid[i_line + 1] if i_line + 1 < len_grid else []
+
+    i_space_prev = max(i_space - 1, 0)
+    i_space_next = min(i_space + 2, len_line)
+
+    line_prev_seg = line_prev[i_space_prev:i_space_next]
+    line_here_seg = line_here[i_space_prev:i_space_next]
+    line_next_seg = line_next[i_space_prev:i_space_next]
+
+    # remove one @ from line_here_seg as it's the current space and
+    # we dont want to account for that
+    for i_char, char in enumerate(line_here_seg):
+        if char == ROLL_OF_PAPER:
+            del line_here_seg[i_char]
+            break
+
+    surrounding = line_prev_seg + line_here_seg + line_next_seg
+
+    roll_count = surrounding.count(ROLL_OF_PAPER)
+
+    can_access = roll_count < 4
+    return can_access
+
+
 def part1(grid: Grid) -> int:
-    answer = 0
-
-    length_grid = len(grid)
-    length_line = len(grid[0])
-
-    # check every space in every line
-    for i_line, line in enumerate(grid):
-
-        line_prev = grid[i_line - 1] if i_line - 1 >= 0 else []
-        line_next = grid[i_line + 1] if i_line + 1 < length_grid else []
-
-        for i_space, space in enumerate(line):
-
-            if space == EMPTY_SPACE:
-                continue
-
-            i_space_prev = max(i_space - 1, 0)
-            i_space_next = min(i_space + 2, length_line)
-
-            line_prev_seg = line_prev[i_space_prev:i_space_next]
-            line_here_seg = line[i_space_prev:i_space_next]
-            line_next_seg = line_next[i_space_prev:i_space_next]
-
-            # remove one @ from line_here_seg as it's the current space and
-            # we dont want to account for that
-            for i_char, char in enumerate(line_here_seg):
-                if char == ROLL_OF_PAPER:
-                    del line_here_seg[i_char]
-                    break
-
-            surrounding = line_prev_seg + line_here_seg + line_next_seg
-
-            roll_count = surrounding.count(ROLL_OF_PAPER)
-
-            can_access = roll_count < 4
-            if can_access:
-                answer += 1
-
-            # print([i_line, i_space], roll_count)
-            # print("    ", line_prev_seg)
-            # print("    ", line_here_seg)
-            # print("    ", line_next_seg)
-            # print()
-
-    return answer
+    len_grid = len(grid)
+    len_line = len(grid[0])
+    return sum(
+        int(can_access_roll(grid, i_line, i_space))
+        for i_space in range(len_line)
+        for i_line in range(len_grid)
+    )
 
 
 def part2(grid: Grid) -> int:
