152753d9602692c8336fbb925a613ec85aaabc59
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-04 07:23:07 +0000

    2025 day 4 part 1
    Squashed commit of the following:
    
    commit 5cb30f56d2556bcc268e72696f774bf566d72a6c
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 07:22:33 2025 +0000
    
        Add part 1 answer+time
    
    commit 40c1e8251e07fc37b7fbff0f05f44857df5b44b0
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 07:21:25 2025 +0000
    
        Remove redundant commented-out line
    
    commit e4336cab5d46fae0599dcd8d96e440ec5eff62f5
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 07:20:47 2025 +0000
    
        Finish part 1
    
    commit 9ce7e9d089dae878442fac051edc9f7ebcff3a3a
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 06:30:24 2025 +0000
    
        Rename "input" to "grid"
    
    commit 778e93cec5ec58c0376754a0748d27727e25ef62
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 06:28:24 2025 +0000
    
        Implement parse_raw_input
    
    commit dd11c1057cc79b07fbab4d1cb294b8c13641bd48
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 06:17:20 2025 +0000
    
        Add example input
    
    commit cf0d74596b5e3e21a4ee98ae9dddb56553d47aec
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Thu Dec 4 06:16:15 2025 +0000
    
        Add files

---
+++ 2025/python/day4.py
@@ -0,0 +1,100 @@
+import os
+import sys
+from typing import Literal
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: https://adventofcode.com/2025/day/4
+# Input URL:   https://adventofcode.com/2025/day/4/input
+
+ROLL_OF_PAPER = "@"
+EMPTY_SPACE = "."
+
+Space = Literal[ROLL_OF_PAPER, EMPTY_SPACE]
+Line = list[Space]
+Grid = list[Line]
+
+
+def parse_raw_input(input: str) -> Grid:
+    # input = """
+    # ..@@.@@@@.
+    # @@@.@.@.@@
+    # @@@@@.@.@@
+    # @.@@@@..@.
+    # @@.@@@@.@@
+    # .@@@@@@@.@
+    # .@.@.@.@@@
+    # @.@@@.@@@@
+    # .@@@@@@@@.
+    # @.@.@@@.@.
+    # """
+    return [[char for char in line.strip()] for line in input.strip().split(os.linesep)]
+
+
+def part1(grid: Grid) -> int:
+    answer = 0
+
+    length_grid = len(grid)
+    length_line = len(grid[0])
+
+    # check every space in every line
+    for i_line, line in enumerate(grid):
+
+        line_prev = grid[i_line - 1] if i_line - 1 >= 0 else []
+        line_next = grid[i_line + 1] if i_line + 1 < length_grid else []
+
+        for i_space, space in enumerate(line):
+
+            if space == EMPTY_SPACE:
+                continue
+
+            i_space_prev = max(i_space - 1, 0)
+            i_space_next = min(i_space + 2, length_line)
+
+            line_prev_seg = line_prev[i_space_prev:i_space_next]
+            line_here_seg = line[i_space_prev:i_space_next]
+            line_next_seg = line_next[i_space_prev:i_space_next]
+
+            # remove one @ from line_here_seg as it's the current space and
+            # we dont want to account for that
+            for i_char, char in enumerate(line_here_seg):
+                if char == ROLL_OF_PAPER:
+                    del line_here_seg[i_char]
+                    break
+
+            surrounding = line_prev_seg + line_here_seg + line_next_seg
+
+            roll_count = surrounding.count(ROLL_OF_PAPER)
+
+            can_access = roll_count < 4
+            if can_access:
+                answer += 1
+
+            # print([i_line, i_space], roll_count)
+            # print("    ", line_prev_seg)
+            # print("    ", line_here_seg)
+            # print("    ", line_next_seg)
+            # print()
+
+    return answer
+
+
+def part2(grid: Grid) -> int:
+    answer = None
+    return answer
+
+
+def main():
+    utils.handle(part1)  # 1505 (0.014733076095581055 seconds)
+    utils.handle(part2)
+
+
+if __name__ == "__main__":
+    main()
