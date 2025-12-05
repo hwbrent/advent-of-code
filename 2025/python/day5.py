c5ad34414b4d76903a2e4880db27fc1a363faa47
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-05 22:16:21 +0000

    2025 day 5 part 1
    Squashed commit of the following:
    
    commit 02919ba8bd0af795b6f23578171aa492ced96e9f
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Fri Dec 5 22:15:51 2025 +0000
    
        Finish part 1
    
    commit 08a9bf738b69d2f5ac2b2f02c2ee9abd267fa284
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Fri Dec 5 22:09:36 2025 +0000
    
        (1) incorrect part 1 answer
    
        no clue what ive done wrong. i got the right answer for the example input...
    
    commit b409f7648894929990d4d4c4cef72375e9c72af8
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Fri Dec 5 22:02:15 2025 +0000
    
        Add example input
    
    commit 7040fea3bd3b57cdd62056015a29a96823639419
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Fri Dec 5 22:01:39 2025 +0000
    
        Implement parse_raw_input
    
    commit db740ec285ce0d02ca6c1a373fd1b6c91e73e688
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Fri Dec 5 08:38:16 2025 +0000
    
        Add files

---
+++ 2025/python/day5.py
@@ -0,0 +1,79 @@
+import os
+import sys
+from typing import Any
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: https://adventofcode.com/2025/day/5
+# Input URL:   https://adventofcode.com/2025/day/5/input
+
+FreshIdRange = list[int, int]
+FreshIdRanges = list[FreshIdRange]
+
+AvailableId = int
+AvailableIds = list[AvailableId]
+
+Input = tuple[FreshIdRanges, AvailableIds]
+
+
+def parse_raw_input(input: str) -> Input:
+    # input = """
+    # 3-5
+    # 10-14
+    # 16-20
+    # 12-18
+
+    # 1
+    # 5
+    # 8
+    # 11
+    # 17
+    # 32
+    # """
+
+    fresh_id_ranges, available_ids = input.strip().split(2 * os.linesep)
+
+    fresh_id_ranges = [
+        [int(num) for num in line.strip().split("-")]
+        for line in fresh_id_ranges.strip().split(os.linesep)
+    ]
+
+    available_ids = [int(num.strip()) for num in available_ids.split(os.linesep)]
+
+    return fresh_id_ranges, available_ids
+
+
+def part1(input: Input) -> int:
+    answer = 0
+
+    fresh_id_ranges, available_ids = input
+
+    for available_id in available_ids:
+        for fresh_id_range in fresh_id_ranges:
+            lower_incl, upper_incl = fresh_id_range
+            if available_id in range(lower_incl, upper_incl + 1):
+                answer += 1
+                break
+
+    return answer
+
+
+def part2(input: Input) -> int:
+    answer = None
+    return answer
+
+
+def main():
+    utils.handle(part1)
+    utils.handle(part2)
+
+
+if __name__ == "__main__":
+    main()
