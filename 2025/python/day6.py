482544e5df598772b1ea5f577600a313fdda87f2
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-06 11:11:36 +0000

    2025 day 6 part 2
    Squashed commit of the following:
    
    commit 3dd76df416e4f05d093dbeeb70446fdb05a22c28
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:58:11 2025 +0000
    
        Reuse part 1 logic in part 2
    
    commit 9d3731b646d1b6da614dbbbdc820e106662f62c3
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:55:53 2025 +0000
    
        Explain nested loop
    
    commit 619d07d15bb49555a58669b90984e243f5cdfbde
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:52:41 2025 +0000
    
        Finish part 2
    
    commit 178b4a751dc8c7de71db5743e1e7106a5f3225df
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:48:12 2025 +0000
    
        Refactor part 1 to use comprehension
    
    commit 640dfba0a42ce76bf1dc15228591c9f2fe60852d
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:47:20 2025 +0000
    
        Return fallback value from operation
    
    commit b0e53eb0ba6b73dc3a5cc2c40b9836afa16e9f60
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:46:38 2025 +0000
    
        Make operation logic reusable
    
    commit e7a3ccbe62283031524cc8b7d0b363ae01575433
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:35:37 2025 +0000
    
        Fix part 1
    
    commit 6e8c3e26d2a3366f13f9f214cd550d28acd4de26
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:34:39 2025 +0000
    
        Do destruction in 'for' line
    
    commit 95b5d22e7bba22dbb15ff3862817a5fae6d316da
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:33:47 2025 +0000
    
        Remove pointless list comprehension
    
    commit eca121db5d9a472158ff0e43d68ac45c53063efc
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:32:01 2025 +0000
    
        Rework parse_raw_input to support part 2
    
    commit 96254476a679a1b49fc6463f48b8c36846458694
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 10:15:12 2025 +0000
    
        Remove unimportant whitespace
    
    commit 2e6a6b64824adcca0e6de4a6137a11efadab73f4
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:42:17 2025 +0000
    
        ignore part 1 for now
    
    commit 04796e1f76810109163d34d00f076948d72e3d34
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:42:02 2025 +0000
    
        add whitespace back to example input
    
    commit 23fd5f56b4fe976d58a20092faa1aa144ed28c89
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:25:34 2025 +0000
    
        Don't automatically parse numbers in input as ints
    
    commit a2cb724148201a8d154ced5948b25e3dea6a5a4c
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:18:33 2025 +0000
    
        Finish part 1
    
    commit 0766156a111cdcce25cb76449da26a043e6bf373
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:17:30 2025 +0000
    
        Get correct result for example input
    
    commit fe9dd60fa280c39dee65fe347176d99b8f027bf2
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:08:50 2025 +0000
    
        Store operands in numpy array
    
    commit 9d6eea9685a6be0173ef6d81a4cfc2ed55aa09f4
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 09:04:03 2025 +0000
    
        Implement parse_raw_input
    
    commit d765ed17a19f56e8dd632b64c25f356f148812ae
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 08:55:07 2025 +0000
    
        Add example input
    
    commit b8895de794ccdb0a6f30e64aea3f36a77e6bb9ca
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sat Dec 6 08:51:02 2025 +0000
    
        Add files

--- 2025/python/day6.py
+++ 2025/python/day6.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import os
 import sys
+import itertools as it
 from pprint import PrettyPrinter
 
 pp = PrettyPrinter(indent=4)
@@ -22,50 +23,96 @@ Problems = list[Problem]
 
 
 def parse_raw_input(input: str) -> Problems:
-    # input = """
+    #     input = """
     # 123 328  51 64
-    # 45 64  387 23
-    # 6 98  215 314
+    #  45 64  387 23
+    #   6 98  215 314
     # *   +   *   +
-    # """
+    #     """
 
-    rows = [row.split() for row in input.strip().split(os.linesep)]
-    cols = [[row[col_number] for row in rows] for col_number in range(len(rows[0]))]
+    # remove any newlines from the left of the input, but not general
+    # whitespace as it could affect the alignment of the numbers
+    input = input.lstrip(os.linesep)
 
-    problems = [
-        (np.array([int(num) for num in cols[i][:-1]]), cols[i][-1])
-        for i in range(len(cols))
-    ]
+    # remove any whitespace at all from the right of the input. this will
+    # only affect the operator line which doesnt make a difference
+    input = input.rstrip()
+
+    rows = input.split(os.linesep)
+
+    # get the indices of the operators. this is where each column starts
+    operator_row = rows[-1]
+    operator_indices = [i for i, char in enumerate(operator_row) if char.strip()]
+
+    # obviously the last column ends at the end of the line. so add the index of the
+    # end of the line so that we can calculate the final range
+    operator_indices.append(len(rows[0]))
+
+    # configure each col's bounds to start at the index of the current
+    # operator and end at the index before the next operator
+    col_bounds = list(it.pairwise(operator_indices))
+
+    problems = []
+    for lower, upper in col_bounds:
+        operands = [row[lower:upper] for row in rows]
+
+        operator = operands.pop().strip()
+
+        problem = [operands, operator]
+
+        problems.append(problem)
 
     return problems
 
 
+to_int = lambda x: int(x.strip())
+map_to_int = np.vectorize(to_int)
+
+
+def do_operation(operands: Operands, operator: Operator) -> int:
+    operands = map_to_int(operands)
+    match operator:
+        case "*":
+            return np.prod(operands)
+        case "+":
+            return np.sum(operands)
+    return 0
+
+
 def part1(problems: Problems) -> int:
-    answer = 0
-
-    for problem in problems:
-        result = 0
-
-        operands, operator = problem
-        match operator:
-            case "*":
-                result = np.prod(operands)
-            case "+":
-                result = np.sum(operands)
-
-        answer += result
-
-    return answer
+    return sum(do_operation(*problem) for problem in problems)
 
 
 def part2(problems: Problems) -> int:
-    answer = 0
-    return answer
+    # remap the operands to the new format, and then use them to basically
+    # repeat part 1
+
+    for i_problem, problem in enumerate(problems):
+        old_operands, _ = problem
+
+        old_operands = [s.replace(" ", "0") for s in old_operands]
+
+        # get new operands by reading each operand right to left and using
+        # each digit
+        new_operands = []
+        length = len(old_operands[0])
+        for i in range(length - 1, -1, -1):  # right to left
+            num_str = "".join([operand[i] for operand in old_operands])
+
+            num_str = num_str.rstrip("0")  # ?
+
+            if num_str != "":
+                new_operands.append(num_str)
+
+        # replace the old operands with the new operands
+        problems[i_problem][0] = new_operands
+
+    return part1(problems)
 
 
 def main():
     utils.handle(part1)  # 5667835681547 (0.009466171264648438 seconds)
-    utils.handle(part2)
+    utils.handle(part2)  # 9434900032651 (0.026433944702148438 seconds)
 
 
 if __name__ == "__main__":
