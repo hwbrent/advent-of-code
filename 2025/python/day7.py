bf26061e3c8ae47bb66a190f8d6777e4f66a8712
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-07 10:23:55 +0000

    2025 day 7 part 1
    Squashed commit of the following:
    
    commit e425bdb1d6fda2744f3ae5e411e59c245eb49eb7
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 10:16:02 2025 +0000
    
        Rerun with printing disabled
    
        A bit faster
    
    commit 979121e8d938fd7369212481116892814d31c108
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 10:15:19 2025 +0000
    
        Finish part 1
    
    commit 154df189ea14d2634c369f44670962ee77d0bbef
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 10:12:59 2025 +0000
    
        Get correct answer for example input
    
    commit 26dd6be961b1a071fe66123f8b62944cee9d92fd
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 10:01:35 2025 +0000
    
        Account for duplicate beams
    
    commit 09ae1f65ec7027eb893f703dfd50b35a54107c33
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 09:56:21 2025 +0000
    
        Only do one mark_position call
    
    commit 2045bdb1bbd8b47fe2c9975d12d9b89517db9601
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 09:49:50 2025 +0000
    
        Print on every iteration
    
    commit 4da772f0fc762bdbbc70e2fe8e49e821c264483e
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 09:49:20 2025 +0000
    
        Append instead of inserting
    
        More performant
    
    commit 96762c49bfc5e485bd3b591fdb1582b2341f5136
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 09:38:01 2025 +0000
    
        Add function to mark beam
    
    commit ff9e12d50acdb4b9fe0b377fcda5b7c382b47378
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 09:29:21 2025 +0000
    
        (1) incorrect part 1 answer
    
        the diagram looks fine at the end but my answer for the example input is 39 whereas the actual answer is 21
    
    commit cbee78f55ca8022935bd0b2255a61b42362b5076
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 08:57:24 2025 +0000
    
        Delineate conditions for splitter having the beam split on it
    
    commit ecea3bbb332f3661c3b9bc4eec0667adf317a3bb
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 08:52:05 2025 +0000
    
        Implement parse_raw_input
    
    commit dce18fb8b357a77094187f74ec8428c5cc66af12
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 08:43:58 2025 +0000
    
        Add example input
    
    commit 470dae3f12cd8f67490b99e2880a6fb1b127417e
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Sun Dec 7 08:42:58 2025 +0000
    
        Create files

---
+++ 2025/python/day7.py
@@ -0,0 +1,132 @@
+import os
+import sys
+from typing import Literal
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+# Enable imports from advent-of-code/utils.py
+root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(root)
+
+import utils
+
+# Problem URL: https://adventofcode.com/2025/day/7
+# Input URL:   https://adventofcode.com/2025/day/7/input
+
+
+START = "S"
+SPLITTER = "^"
+EMPTY_SPACE = "."
+BEAM = "|"
+
+
+Space = Literal[START, SPLITTER, EMPTY_SPACE]
+Row = list[Space]
+Manifold = list[Row]
+
+
+def parse_raw_input(input: str) -> Manifold:
+    # input = """
+    # .......S.......
+    # ...............
+    # .......^.......
+    # ...............
+    # ......^.^......
+    # ...............
+    # .....^.^.^.....
+    # ...............
+    # ....^.^...^....
+    # ...............
+    # ...^.^...^.^...
+    # ...............
+    # ..^...^.....^..
+    # ...............
+    # .^.^.^.^.^...^.
+    # ...............
+    # """
+    return [[value for value in row.strip()] for row in input.strip().split(os.linesep)]
+
+
+def mark_position(manifold, row, col):
+    if manifold[row][col] == EMPTY_SPACE:
+        manifold[row][col] = BEAM
+
+
+def part1(manifold: Manifold) -> int:
+    split_on = set()
+
+    pos_start = next(
+        [i_row, i_col]  # needs to be mutable
+        for i_row, row in enumerate(manifold)
+        for i_col, value in enumerate(row)
+        if value == START
+    )
+
+    beams = [pos_start]
+
+    while len(beams) > 0:
+
+        # iterate over the beams
+        i_beam = 0
+        while i_beam < len(beams):
+            beam = beams[i_beam]
+            row, col = beam
+
+            mark_position(manifold, row, col)
+
+            space = manifold[row][col]
+
+            # if the beam is currently on a splitter
+            if space == SPLITTER:
+                # mark this splitter as having split the beam
+                split_on.add(tuple(beam))
+
+                left_beam = [row, col - 1]
+                right_beam = [row, col + 1]
+
+                # account for duplicate, overlapping beams
+
+                if left_beam in beams:
+                    del beams[i_beam]
+                else:
+                    # repurpose the current beam, which is on the splitter,
+                    # into the left beam
+                    beams[i_beam] = left_beam
+
+                # if we don't already have a beam where the right beam would
+                # be, add it
+                if not right_beam in beams:
+                    beams.append(right_beam)
+
+            # if the beam is currently on the bottom row
+            if row == len(manifold) - 1:
+                # it's done moving, so remove it
+                del beams[i_beam]
+                continue
+
+            # move the current beam down
+            beam[0] += 1
+
+            i_beam += 1
+
+        # print(os.linesep)
+        # for row in manifold:
+        #     print(*row)
+        # print(os.linesep)
+
+    return len(split_on)
+
+
+def part2(manifold: Manifold) -> int:
+    answer = 0
+    return answer
+
+
+def main():
+    utils.handle(part1)  # 1622 (0.5911369323730469 seconds)
+    utils.handle(part2)
+
+
+if __name__ == "__main__":
+    main()
