51ba77004a8c936adb97b233f9f00542198e882f
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-22 12:08:01 +0000

    2025 day 1 part 2
    Squashed commit of the following:
    
    commit 2ed5999a6296aec63e087165fbbe2ef379a0076c
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 22 12:06:13 2025 +0000
    
        Make day 1 answer times line up
    
    commit 42f3888e2ed22acd4ffba1c46cb46bd1dfe770cb
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 22 12:05:23 2025 +0000
    
        Add part 2 answer and time
    
    commit 7170ae1fac0ab6a8c67130310dbf083aa7e43df4
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 22 12:05:00 2025 +0000
    
        Finish part 2
    
    commit 661bf21e5536ff8b8e0a2a2496b422166c601f50
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 22 11:37:15 2025 +0000
    
        Add URLs
    
    commit 08802c5aa35279452478113e62c89f4ddd80ef4e
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 22 11:34:53 2025 +0000
    
        Clear existing part 2 logic
    
    commit 79b642ceba6e6590faf9de5243a1d7d925811e05
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 09:32:16 2025 +0000
    
        (4) Part 2 incorrect answer
    
        5765
    
    commit 05d1e149cb6cb62cdffd6e2dcd60b6d52e012de9
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Tue Dec 2 09:01:45 2025 +0000
    
        Don't reassign dial_value
    
    commit aa49e8913840eecc4da893e109120df1077c8332
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 18:15:10 2025 +0000
    
        (3) Part 2 incorrect answer
    
    commit 14c8cfade7f5dc4e52e08f0de1b62e0424e70e62
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 18:11:41 2025 +0000
    
        (2) Part 2 incorrect answer
    
    commit 8da86274858605b108a83f02d198f77060f46344
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 18:07:35 2025 +0000
    
        Part 2 incorrect answer
    
    commit 4f276e8a43bcce7b621ee9613ace107a50a79acf
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:59:17 2025 +0000
    
        Finish part 1
    
    commit b8590d56fcc9a272de6da0b11fa70bc1b4a33abd
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:53:45 2025 +0000
    
        Implement parse_raw_input
    
    commit 634d4ea2b36f7bd114f4f4549ba790a796368f5e
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:51:44 2025 +0000
    
        Disable inline copilot suggestions
    
    commit 6cdd28b6212cd4387e20db088e223a6abfc618a3
    Author: hwbrent <68196383+hwbrent@users.noreply.github.com>
    Date:   Mon Dec 1 17:46:58 2025 +0000
    
        Create files
    
        I tried the script once but it didnt work. i didnt try again cos the creator asked on reddit for us to not make loads of automated requests to the server. so i just manually copied this stuff over. not too much of a hassle

--- 2025/python/day1.py
+++ 2025/python/day1.py
@@ -74,6 +74,36 @@ Following these rotations would cause the dial to move as follows:
 Because the dial points at 0 a total of three times during this process, the password in this example is 3.
 
 Analyze the rotations in your attached document. What's the actual password to open the door?
+
+--- Part Two ---
+
+You're sure that's the right password, but the door won't open. You knock, but nobody answers. You build a snowman while you think.
+
+As you're rolling the snowballs for your snowman, you find another security document that must have fallen into the snow:
+
+"Due to newer security protocols, please use password method 0x434C49434B until further notice."
+
+You remember from the training seminar that "method 0x434C49434B" means you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+
+Following the same rotations as in the above example, the dial points at zero a few extra times during its rotations:
+
+    The dial starts by pointing at 50.
+    The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+    The dial is rotated L30 to point at 52.
+    The dial is rotated R48 to point at 0.
+    The dial is rotated L5 to point at 95.
+    The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+    The dial is rotated L55 to point at 0.
+    The dial is rotated L1 to point at 99.
+    The dial is rotated L99 to point at 0.
+    The dial is rotated R14 to point at 14.
+    The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+
+In this example, the dial points at 0 three times at the end of a rotation, plus three more times during a rotation. So, in this example, the new password would be 6.
+
+Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial to point at 0 ten times before returning back to 50!
+
+Using password method 0x434C49434B, what is the password to open the door?
 """
 
 
@@ -82,7 +112,7 @@ TOTAL_DIAL_VALUES = 100
 
 
 def parse_raw_input(input: str):
-    #     input = """
+    # input = """
     # L68
     # L30
     # R48
@@ -129,13 +159,30 @@ def part1(input):
 
 
 def part2(input):
-    answer = None
+    answer = 0
+
+    dial_value = INITIAL_DIAL_VALUE
+
+    # for each rotation, apply it one step at a time, and after each step,
+    # if the dial is on zero, increment the answer
+
+    for distance in input:
+
+        step = 1 if distance > 0 else -1
+
+        for _ in range(abs(distance)):
+            dial_value += step
+            dial_value %= TOTAL_DIAL_VALUES  # wrap around
+
+            if dial_value == 0:
+                answer += 1
+
     return answer
 
 
 def main():
-    utils.handle(part1)  # 989 (0.0020248889923095703 seconds)
-    utils.handle(part2)
+    utils.handle(part1)  # 989  (0.0020248889923095703 seconds)
+    utils.handle(part2)  # 5941 (0.041471004486083984 seconds)
 
 
 if __name__ == "__main__":
