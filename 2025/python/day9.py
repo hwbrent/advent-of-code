6be9b60b6335f96bd81613a4d778c02c5a19af12
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-12-22 13:06:24 +0000

    Finish part 1

--- 2025/python/day9.py
+++ 2025/python/day9.py
@@ -23,16 +23,16 @@ Tiles = list[Tile]
 
 
 def parse_raw_input(input: str) -> Tiles:
-    input = """
-    7,1
-    11,1
-    11,7
-    9,7
-    9,5
-    2,5
-    2,3
-    7,3
-    """
+    # input = """
+    # 7,1
+    # 11,1
+    # 11,7
+    # 9,7
+    # 9,5
+    # 2,5
+    # 2,3
+    # 7,3
+    # """
     return [
         [int(num) for num in line.strip().split(",")]
         for line in input.strip().split(os.linesep)
@@ -41,7 +41,24 @@ def parse_raw_input(input: str) -> Tiles:
 
 def part1(tiles: Tiles) -> int:
     answer = 0
-    print(tiles)
+
+    for tile1 in tiles:
+        for tile2 in tiles:
+            if tile1 == tile2:
+                continue
+
+            row1, col1 = tile1
+            row2, col2 = tile2
+
+            # +1 to account for if the row/col is the same for both - one
+            # row/col still counts as a row of red tiles and therefore takes
+            # up area
+            row_diff = row2 - row1 + 1
+            col_diff = col2 - col1 + 1
+            area = row_diff * col_diff
+
+            answer = max(answer, area)
+
     return answer
 
 
@@ -51,7 +68,7 @@ def part2(tiles: Tiles) -> int:
 
 
 def main():
-    utils.handle(part1)
+    utils.handle(part1)  # 4781546175 (0.09149479866027832 seconds)
     utils.handle(part2)
 
 
